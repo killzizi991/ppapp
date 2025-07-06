3334d4d00bb58e939c3d111f22c4773b8ee1c49d
killzizi991 <killzizi@yandex.ru>
2025-07-06 09:21:23 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'finance-tracker-v1';
+const OFFLINE_URL = 'offline.html';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'styles.css',
+  'app.js',
+  'manifest.webmanifest',
+  'https://cdnjs.cloudflare.com/ajax/libs/date-fns/2.29.3/date_fns.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Кэширование основных ресурсов');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match(OFFLINE_URL))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
